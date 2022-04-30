dd35d62f373db50eaafbbac053c42b99efcf484b
MikhailVinogradov224 <m89119750476@gmail.com>
2022-04-30 04:00:46 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿            int[] arr = new int[15];
+            int k = 0;
+            Random rnd = new Random();
+ 
+            
+            for (int i = 0; i < 15; i++)
+            {
+                arr[i] = rnd.Next(100, 999);
+                Console.Write("{0} ", arr[i]);
+            }
+            
+ 
+            foreach(int element in arr)
+            {
+                
+                if (element % 2 == 0)
+                {
+                    k++;
+                    
+                }
+            }
+          
+            Console.WriteLine();
+            Console.WriteLine(k);
